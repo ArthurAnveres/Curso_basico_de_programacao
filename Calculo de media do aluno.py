d38b80134480335707bcca98508fbd51359939d2
ArthurAnveres <126812233+ArthurAnveres@users.noreply.github.com>
2023-06-28 09:21:32 -0400

    Create Calculo de media do aluno.py

---
+++ Calculo de media do aluno.py
@@ -0,0 +1,7 @@
+nome=input("Insira o nome do aluno: ")
+nota1=float(input("Ensira a primeira nota: "))
+nota2=float(input("Ensira a segunda nota: "))
+
+media = (nota1 + nota2) /2
+
+print(f"O aluno {nome} está com {media:_.2f} de média")
