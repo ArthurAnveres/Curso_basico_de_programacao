7fe3bc6e50a8b6f46a65a6277525ea5322a0ff91
ArthurAnveres <126812233+ArthurAnveres@users.noreply.github.com>
2023-06-28 09:18:59 -0400

    Create Tabuada.py

---
+++ Tabuada.py
@@ -0,0 +1,7 @@
+print("***************************")
+print("*         Tabuada         *")
+print("***************************")
+numero=int(input("Digite um número para criar a tabuada: "))
+
+for n in range(1,11):
+    print(n,"x",numero,"=",numero*n)
