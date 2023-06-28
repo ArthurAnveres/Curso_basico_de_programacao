087c451ed986003f1e875de5f6e80b4ed5ffb18b
ArthurAnveres <126812233+ArthurAnveres@users.noreply.github.com>
2023-06-28 11:45:48 -0400

    Update Selecionamento de opções.py

--- Selecionamento de opções.py
+++ Selecionamento de opções.py
@@ -1,1 +1,20 @@
-
+while True:
+    print("1-cadastrar")
+    print("2-alterar")
+    print("3-Apagar")
+    print("4-mostrar")
+    print("5-sair")
+    escolha=input("Escolha uma opção!")
+    if escolha=="1":
+        print("Cadastro efetuado com sucesso!!!")
+    elif escolha=="2":
+        print("Cadastro alterado com sucesso!!!")
+    elif escolha=="3":
+        print("Cadastro apagado com sucesso")
+    elif escolha=="4":
+        print("Cadastro mostrado com sucesso!!!")
+    elif escolha=="5":
+        print("Desligando sistema saida bem sucedida")
+        break
+    else:
+        print("Você digitou um valor fora da faixa 1-5!!!!")
